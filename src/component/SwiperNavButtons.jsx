8d89bfa69d326e218caa8a702489f2fb0f417ed9
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-03-23 18:27:27 +0500

    update

--- src/component/SwiperNavButtons.jsx
+++ src/component/SwiperNavButtons.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useSwiper } from 'swiper/react';
-
+import { IoCaretForwardOutline,IoCaretBack  } from "react-icons/io5";
 export const SwiperNavButtons = () => {
   const swiper = useSwiper();
 
   return (
     <div className="swiper-nav-btns">
-      <button onClick={() => swiper.slidePrev()}>Prev</button>
+   <div>
+    <span onClick={() => swiper.slidePrev()}>Next</span>
+   <button ><IoCaretForwardOutline /></button>
+   </div>
       <span>View more</span>
-      <button onClick={() => swiper.slideNext()}>Next</button>
+      <div>
+        <span onClick={() => swiper.slideNext()}>back</span>
+      <button ><IoCaretBack /></button>
+      </div>
     </div>
   );
 };
