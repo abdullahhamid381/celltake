fdc30a84a289850c0e1f1d052e66aa6a683e796f
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-03-23 22:14:42 +0500

    update

--- src/component/Home/TopSell.jsx
+++ src/component/Home/TopSell.jsx
@@ -81,7 +81,7 @@ function App() {
             spaceBetween: 20,
           },
           768: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             spaceBetween: 40,
           },
           1024: {
